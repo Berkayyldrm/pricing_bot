8b4d082421892b6eccbc550a95eb17af1861b9dc
gcloud <gcloud@email.com>
2024-12-21 19:25:52 +0000

    typo

--- price_data_sender.py
+++ price_data_sender.py
@@ -18,14 +18,13 @@ def publish_message(message):
     connection.close()
 
 def get_response_from_url(url):
-    print(url)
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
         "Cache-Control": "no-cache",
         "Pragma": "no-cache",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     }
     cookies = {
         "gcl_au": "1.1.581871697.1732298393",
